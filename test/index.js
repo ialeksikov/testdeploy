48e386a26600c44d9c9e497672d8ec3a9fc54476
ialeksikov <igor.aleksikov99@gamil.com>
2019-06-15 14:07:58 +0300

    2

--- test/index.js
+++ test/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const Telegraf = require('telegraf');
+const app = new Telegraf("886750169:AAF40utD9rfqf5hB6OhbOaH7s6enYaXJwMg");
+
+app.hears('hi', ctx => {
+ return ctx.reply('What is up?');
+});
+
+app.startPolling();

784f02f7856883d8dddf5c6bd96a537160160530
ialeksikov <igor.aleksikov99@gamil.com>
2019-06-17 15:24:19 +0300

    100

--- test/index.js
+++ test/index.js
@@ -1,29 +1,7 @@
 'use strict'
-// const Telegraf = require('telegraf');
-// const app = new Telegraf("886750169:AAF40utD9rfqf5hB6OhbOaH7s6enYaXJwMg");
-
-// app.hears('hi', ctx => {
-//  return ctx.reply('What is up?');
-// });
-
-// app.startPolling();
-
-//let dictionaryStorage = [];
-
 var request = require('request');
 var admin = require("firebase-admin");
-var functions = require("firebase-functions");
-var express = require("express");
-const app = express();
-app.get('/timestamp', (request, response)=>
-    {
-        response.send('${Date.now()}');
-    }
-);
-
 var serviceAccount = require("../myfirst-23ce4-firebase-adminsdk-htwji-6267dfbb17.json");
-
-exports.startConnection = functions.https.onRequest(app);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -31,12 +9,9 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -61,7 +36,8 @@ const bot = new TelegramBot(token,
     );
     //${token}
     //use URL + TOKEN for secure connection
-    const webHookUrl = `https://testdeploy.igoraleksikov99.now.sh`; //?${token}
+    // const webHookUrl = `https://testdeploy.igoraleksikov99.now.sh`; //?${token}
+    const webHookUrl = `https://cadc2053.ngrok.io/${token}`; //?${token}
 
     const setWebHook = () => {
         const setWebhookUrl = `https://api.telegram.org/bot${token}/setWebhook`;
@@ -81,54 +57,46 @@ const bot = new TelegramBot(token,
       
       //Call me to set a webhook
       setWebHook();
-      setInterval(function(){
-        bot.getWebHookInfo().then((data) =>{
-            console.log(data);
-        });
+    //   setInterval(function(){
+    //     bot.getWebHookInfo().then((data) =>{
+    //         console.log(data);
+    //     });
 
-      }, 5000);
+    //   }, 5000);
 //Bot messaging logic
 bot.on('message', (msg) => {
-  //const groupId = -377348263;
-  //const adminId = 604941595;
-  console.log(msg);
-//   bot.sendMessage(adminId, `${msg.from.id}`);
   if(msg.chat.type == "group" || msg.chat.type == "supergroup"){
-        if(msg.reply_to_message && (msg.text == "+" || msg.text == "-") && msg.from.id != msg.reply_to_message.from.id){
-            //dictionaryStorage[updateMsg.reply_to_message.from.id][updateMsg.chat.id].value ++;
-            //console.dir(dictionaryStorage);
-            //console.log(1);
+        if(msg.reply_to_message && (msg.text == "+" || msg.text == "-")){// && msg.from.id == msg.reply_to_message.from.id){
             updateUserScore(msg);
-              
         }
     }else if(msg.chat.type == "private"){
         if(msg.text == "/score")
         sendUserScore(msg);
         else sendHelpMessage(msg);
-        
-    }
-  
-  
-  
+    }  
 });
-
-
+ 
 //Function prototypes
 function getUserScore(msg){
-    return Math.round(Math.random()*20);
+    var score = 0;
+    db.collection("usersReply").get("chat").then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log(doc.data()["text"]);//["from"]);
+          if(doc.data()["text"] === "+") score++;
+            else score--;
+            console.log(score);
+        });
+      });
+    return score;
 }
+
 function sendUserScore(msg){
     let score = getUserScore(msg);
     bot.sendMessage(msg.from.id, `${msg.from.first_name}, your score is ${score}`);
 }
 function updateUserScore(msg){
-    // db.collection("usersReply").add({
-    //     chat: msg.chat.id,
-    //     user: msg.from.id,
-    //     value: msg.text
-    // }).then(()=>{
-    //     bot.sendMessage(adminId, `User with ID [${msg.from.id}] has just replied to user with ID[${msg.reply_to_message.from.id}] with the following message: ${msg.text}`);
-    // });
+    var data = msg;
+     db.collection("usersReply").add(data);
     bot.sendMessage(msg.reply_to_message.from.id, `Your score was ${msg.text == "+" ? "increased" : "descreased"}`);
 }
 
